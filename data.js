2a35cf1bbe360c688b0eae3bce9b09a16eb5429e
Louisa Canepa <l.canepa@unsw.edu.au>
2024-08-22 15:44:58 +1000

    experimenting with timeline

--- data.js
+++ data.js
@@ -10,50 +10,67 @@ export const bio = [
      optical survey images."
 ];
 
-export const skills = [
+export const experience = [
     {
-        title: "Languages",
-        skillName: "JavaScript, TypeScript",
-        color: "1",
-        percentage: "80",
+        title: "Shiprocket (Bigfoot Solution Private Limited)",
+        duration: "September 2022 - Present",
+        subtitle: "Software Engineer",
+        details: [
+            "Working in support and escalation team."
+        ],
+        tags: ["JavaScript", "Angular", "Bootstrap", "Nodejs", "Jenkins"],
+        icon: "truck ",
     },
     {
-        title: "Frameworks/Libraries",
-        skillName: "Angular, RxJS, NGXS",
-        color: "6",
-        percentage: "70",
+        title: "Biofourmis India Private Limited",
+        duration: "April 2022 - Jul 2023",
+        subtitle: "Software Engineer",
+        details: [
+            `Implemented microfrontends using the Module Federation Plugin in Angular.
+            Rendered components from remote modules/repositories, enabling a modular and scalable architecture`,
+            `Upgraded application with a dynamic base URL for flexible operation across multiple regions and verticals, leading to
+            recognition with the Biofourmis Bravo award in Q1, 2023.`,
+            `Implemented RxState for managing local state in components, resulting in elegant and reactive facades.`,
+            `Utilized Twilio-Video for group video call rooms and Countly for Web Analytics.`,
+            `Integrated NGXS WebSocket, enhancing state management and replacing legacy service injections.`,
+            `Implemented extensive RxJS usage for reactive programming, resulting in efficient data handling, improved state
+            management, and enhanced application performance.`,
+            `Dramatically optimized load time and performance through code splitting, lazy loading, caching, and preload`,
+            `Enhanced video call experience by implementing real-time switching of I/O devices and audio level indicators,
+            resulting in reduced audio issues and increased user satisfaction.`,
+            `Owned and led successful Angular application upgrades from version 12 to 13 and 14, improving performance
+            and enhancing features.`,
+        ],
+        tags: ["JavaScript", "Angular", "RxJS", "NGXS", "TypeScript", "RxState", "Webpack", "Optimization"],
+        icon: "heartbeat",
     },
     {
-        title: "Design",
-        skillName: "HTML, Bootstrap, Tailwind",
-        color: "4",
-        percentage: "70",
+        title: "Novopay Solutions Private Limited",
+        duration: "June 2020 - April 2022",
+        subtitle: "Software Engineer",
+        details: [
+            `Implemented AePS Services, including Bio-metric eKYC, Cash Withdrawal, Balance Enquiry, Mini-Statements,
+            and Money transfer.`,
+            `Implemented QR Code integration for retailer wallet money loading, Clevertap events for user action tracking,
+            Freshdesk ticketing, Chatbot services, and Retailer onboarding/admin interfaces.`,
+            `Developed retailer QR code system for seamless money loading into wallets, resulting in improved user transactions.`,
+            `Achieved 70 percent code coverage of the entire application by writing unit tests for legacy code.`,
+        ],
+        tags: ["JavaScript", "Angular", "Bootstrap", "Nodejs", "Jenkins"],
+        icon: "qrcode",
     },
     {
-        title: "Version Control",
-        skillName: "GitHub, JIRA, Bitbucket",
-        color: "7",
-        percentage: "70",
-    },
-    {
-        title: "Tools",
-        skillName: "Postman, Chrome DevTools",
-        color: "3",
-        percentage: "80",
-    },
-    {
-        title: "Analytics",
-        skillName: "CleverTap, Countly",
-        color: "5",
-        percentage: "50",
-    },
-    {
-        title: "IDE",
-        skillName: "VS Code",
-        color: "6",
-        percentage: "70",
+        title: "ThinkPedia LLP",
+        duration: "May 2019 - June 2019",
+        subtitle: "SDE Intern",
+        details: [
+            `Developed a customer web application for social media management, supporting the advertisement domain.`,
+        ],
+        tags: ["JavaScript", "Angular", "Bootstrap", "Java", "Spring Boot"],
+        icon: "group",
     },
 ];
+
 
 export const projects = {
     webProjects: [
@@ -153,67 +170,6 @@ export const projects = {
         },
     ],
 };
-
-export const experience = [
-    {
-        title: "Shiprocket (Bigfoot Solution Private Limited)",
-        duration: "September 2022 - Present",
-        subtitle: "Software Engineer",
-        details: [
-            "Working in support and escalation team."
-        ],
-        tags: ["JavaScript", "Angular", "Bootstrap", "Nodejs", "Jenkins"],
-        icon: "truck ",
-    },
-    {
-        title: "Biofourmis India Private Limited",
-        duration: "April 2022 - Jul 2023",
-        subtitle: "Software Engineer",
-        details: [
-            `Implemented microfrontends using the Module Federation Plugin in Angular.
-            Rendered components from remote modules/repositories, enabling a modular and scalable architecture`,
-            `Upgraded application with a dynamic base URL for flexible operation across multiple regions and verticals, leading to
-            recognition with the Biofourmis Bravo award in Q1, 2023.`,
-            `Implemented RxState for managing local state in components, resulting in elegant and reactive facades.`,
-            `Utilized Twilio-Video for group video call rooms and Countly for Web Analytics.`,
-            `Integrated NGXS WebSocket, enhancing state management and replacing legacy service injections.`,
-            `Implemented extensive RxJS usage for reactive programming, resulting in efficient data handling, improved state
-            management, and enhanced application performance.`,
-            `Dramatically optimized load time and performance through code splitting, lazy loading, caching, and preload`,
-            `Enhanced video call experience by implementing real-time switching of I/O devices and audio level indicators,
-            resulting in reduced audio issues and increased user satisfaction.`,
-            `Owned and led successful Angular application upgrades from version 12 to 13 and 14, improving performance
-            and enhancing features.`,
-        ],
-        tags: ["JavaScript", "Angular", "RxJS", "NGXS", "TypeScript", "RxState", "Webpack", "Optimization"],
-        icon: "heartbeat",
-    },
-    {
-        title: "Novopay Solutions Private Limited",
-        duration: "June 2020 - April 2022",
-        subtitle: "Software Engineer",
-        details: [
-            `Implemented AePS Services, including Bio-metric eKYC, Cash Withdrawal, Balance Enquiry, Mini-Statements,
-            and Money transfer.`,
-            `Implemented QR Code integration for retailer wallet money loading, Clevertap events for user action tracking,
-            Freshdesk ticketing, Chatbot services, and Retailer onboarding/admin interfaces.`,
-            `Developed retailer QR code system for seamless money loading into wallets, resulting in improved user transactions.`,
-            `Achieved 70 percent code coverage of the entire application by writing unit tests for legacy code.`,
-        ],
-        tags: ["JavaScript", "Angular", "Bootstrap", "Nodejs", "Jenkins"],
-        icon: "qrcode",
-    },
-    {
-        title: "ThinkPedia LLP",
-        duration: "May 2019 - June 2019",
-        subtitle: "SDE Intern",
-        details: [
-            `Developed a customer web application for social media management, supporting the advertisement domain.`,
-        ],
-        tags: ["JavaScript", "Angular", "Bootstrap", "Java", "Spring Boot"],
-        icon: "group",
-    },
-];
 
 export const education = [
     {
